e65475005bbcebf2bc7821da7696f1b09b0b969a
solivanlau <solivan_lau@outlook.com>
2023-06-29 16:49:15 -0400

    simple server listening

--- server/app.js
+++ server/app.js
@@ -1,1 +1,17 @@
+require('dot-env');
+const express = require('express');
+const app = express();
 
+// app init
+const port = process.env.PORT || 3000;
+const start = async () => {
+  try {
+    app.listen(port, () => {
+      console.log(`server is listening on port, ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
